e739f82fd133a87aec4b0043fb19ace232cbb383
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-15 12:08:53 +1000

    person-class

---
+++ person-class.js
@@ -0,0 +1,15 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.name = {}
+    this.name.first = firstName
+    this.name.last = lastName
+  }
+
+  fullName() {
+    return this.name.first + ' ' + this.name.last
+  }
+
+}
+
+person = new Person('Jonh', 'Citizen')
+console.log(person.fullName())
