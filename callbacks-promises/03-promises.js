df476dc098f7df957cfbf63859a65a230aba98ee
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-21 12:30:36 +1000

    promises

--- callbacks-promises/03-promises.js
+++ callbacks-promises/03-promises.js
@@ -1,13 +1,21 @@
-let promise = new Promise((resolve, reject) => {
-  const result = Math.random
-  if (result > 0.5) {
-      resolve(result)
-  } else {
-      reject()
-  }
-})
+(function() {
 
-promise.then(
-    (value) => { console.log(`resolved to ${value}`) },
-    () => { console.log('rejected') }    
-)
+    let promise = new Promise((resolve, reject) => {
+        const result = Math.random
+        if (result > 0.5) {
+            resolve(result)
+        } else {
+            reject("You've done something wrong!")
+        }
+    })
+
+    promise.then(
+        (value) => { console.log(`resolved to ${value}`) },
+        () => { console.log('rejected') }    
+    )
+
+    promise.then(
+        (value) => { return value * 100 }
+    )
+
+})()
