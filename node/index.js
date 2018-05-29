02293ed03be4c50e32a598be05dd0217df405bde
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-29 12:12:55 +1000

    create server

--- node/index.js
+++ node/index.js
@@ -1,6 +1,8 @@
 const _ = require('loadash')
 const { students, teachers } = require('./app')
 const fs = require('fs')
+const http = require('http')
+const port = 4000
 
 students.forEach( student => {
   console.log(student)
@@ -15,3 +17,15 @@ fs.readFile('./package.jon', (err, data) => {
   console.log(data.constructor.name)
   console.log(data);
 });
+
+const server = http.createServer(function(req, resp) {
+  console.log(req.url)
+  resp.end('Hello World!')
+})
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('Error! ' + err)
+  }
+  console.log(`Server started on port ${port}`)
+})
