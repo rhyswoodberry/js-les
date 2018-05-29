194d3b7ee63e09fea801044388559d14a7b63613
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-29 12:06:22 +1000

    npm

--- node/index.js
+++ node/index.js
@@ -1,5 +1,6 @@
 const _ = require('loadash')
 const { students, teachers } = require('./app')
+const fs = require('fs')
 
 students.forEach( student => {
   console.log(student)
@@ -8,3 +9,9 @@ students.forEach( student => {
 teachers.forEach(teacher => {
   console.log(teacher)
 })
+
+fs.readFile('./package.jon', (err, data) => {
+  if (err) throw err; 
+  console.log(data.constructor.name)
+  console.log(data);
+});
