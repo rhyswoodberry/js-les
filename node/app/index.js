d9d402a663171b4c1a21e7dddaeb3ff54725949d
Rhys Woodberry <rhysbrian@gmail.com>
2018-05-29 11:02:50 +1000

    module export

--- node/app/index.js
+++ node/app/index.js
@@ -1,2 +1,5 @@
 const students = [{ id: 666, first_name: 'Jane', last_name: 'Smyth' }]
 const teachers = [{ id: 888, first_name: 'Tom', last_name: 'Gordon' }]
+
+module.exports.students = students
+module.exports.teachers = teachers
