25e1b4f60a82df004512b4a5da6c862b8f87af74
Rhys Woodberry <rhysbrian@gmail.com>
2018-06-01 10:55:30 +1000

    authenticate

--- sequelizejs-app/index.js
+++ sequelizejs-app/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const studentsRouter = require('express');
+const sequelize = require('./db')
+
 const app = express();
 const port = 4000;
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.use(bodyParser.json());
 
